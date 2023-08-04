0c967ff8d0a4ea703dba34789d3a2d7e784aca7e
Barla Ankit <barla.x88@gmail.com>
2023-08-04 22:50:15 +0530

    Create menu to generate new grid

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const rangeInput = document.querySelector('#gridSize');
+const size = document.querySelector('#size');
+const createBtn = document.querySelector('#create');
+let container;
+
 const createGrid = (size) => {
   const containerDiv = document.createElement('div');
   const rowDiv = document.createElement('div');
@@ -10,22 +15,58 @@ const createGrid = (size) => {
 
   //create squares inside rows
   for (let i = 1; i <= size; i++) {
-    rowDiv.appendChild(childDiv.cloneNode(true));
+    const square = childDiv.cloneNode(true);
+    rowDiv.appendChild(square);
   }
 
   for (let i = 1; i <= size; i++) {
     containerDiv.appendChild(rowDiv.cloneNode(true));
   }
+
+  //setup listners
+  //turn squares black on click
+  containerDiv.addEventListener('click', (e) => {
+    if (!e.target.classList.contains('square')) return;
+
+    e.target.style.backgroundColor = 'black';
+  });
+
   return containerDiv;
 };
 
-const container = createGrid(16);
+const setHover = () => {
+  const elements = document.querySelectorAll('.square');
 
-document.body.prepend(container);
+  elements.forEach((square) => {
+    //highlight squares on mouse hover
+    //add eventlistener for highlighting squares on mouse hover
+    square.addEventListener('mouseenter', () => {
+      square.classList.toggle('mousehover');
+    });
 
-container.addEventListener('click', (e) => {
-  if (!e.target.classList.contains('square')) return;
+    square.addEventListener('mouseleave', () => {
+      square.classList.toggle('mousehover');
+    });
+  });
+};
 
-  console.log('run');
-  e.target.style.backgroundColor = 'black';
-});
+const init = () => {
+  size.textContent = `${rangeInput.value} X ${rangeInput.value}`;
+  container = createGrid(16);
+  document.querySelector('.app').prepend(container);
+  setHover();
+
+  //listners
+  rangeInput.addEventListener('change', () => {
+    size.textContent = `${rangeInput.value} X ${rangeInput.value}`;
+  });
+
+  createBtn.addEventListener('click', () => {
+    container.remove();
+    container = createGrid(Number(rangeInput.value));
+    document.querySelector('.app').prepend(container);
+    setHover();
+  });
+};
+
+init();
